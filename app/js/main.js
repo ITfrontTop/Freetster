cc22174967303524a67f1b11f78c50986b390e6f
ITfrontTop <Fine1337@ukr.net>
2023-05-31 13:09:20 +0300

    fixet error

--- app/js/main.js
+++ app/js/main.js
@@ -26,6 +26,13 @@ $(function () {
           ]
     });
 
+    $(".menu a").on("click", function (e) {
+		e.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
     $('.filters__btn').on('click', function(){
         $('.filters').toggleClass('filters--active');         
         $('.filters__btn').toggleClass('filters__btn--active');
